79ac41c12a4b7b02d2585581f66de8e060ff6c45
ECE183DA-AWDFRW <36172219+ECE183DA-AWDFRW@users.noreply.github.com>
2018-03-09 17:29:08 -0800

    Add files via upload
    attempted to add pwm function

--- paperbot/paperbot.ino
+++ paperbot/paperbot.ino
@@ -1,3 +1,30 @@
+/*
+  Wireless Servo Control, with ESP as Access Point
+  Usage: 
+    Connect phone or laptop to "ESP_XXXX" wireless network, where XXXX is the ID of the robot
+    Go to 192.168.4.1. 
+    A webpage with four buttons should appear. Click them to move the robot.
+  Installation: 
+    In Arduino, go to Tools > ESP8266 Sketch Data Upload to upload the files from ./data to the ESP
+    Then, in Arduino, compile and upload sketch to the ESP
+  Requirements:
+    Arduino support for ESP8266 board
+      In Arduino, add URL to Files > Preferences > Additional Board Managers URL.
+      See https://learn.sparkfun.com/tutorials/esp8266-thing-hookup-guide/installing-the-esp8266-arduino-addon
+    Websockets library
+      To install, Sketch > Include Library > Manage Libraries... > Websockets > Install
+      https://github.com/Links2004/arduinoWebSockets
+    
+    ESP8266FS tool
+      To install, create "tools" folder in Arduino, download, and unzip. See 
+      https://github.com/esp8266/Arduino/blob/master/doc/filesystem.md#uploading-files-to-file-system
+  Hardware: 
+  * NodeMCU Amica DevKit Board (ESP8266 chip)
+  * Motorshield for NodeMCU 
+  * 2 continuous rotation servos plugged into motorshield pins D1, D2
+  * Ultra-thin power bank 
+  * Paper chassis
+*/
 #include <Arduino.h>
 
 #include <Hash.h>
@@ -554,7 +581,12 @@ void webSocketEvent(uint8_t id, WStype_t type, uint8_t * payload, size_t length)
             DEBUG("On connection #", id)
             DEBUG("  got text: ", (char *)payload);
 
-            if (payload[0] == '#') {
+            if (payload[0] == '$') {
+                int left_motor = 100*(payload[1]-0) + 10*(payload[2]-0) + payload[3]-0;
+                int right_motor = 100*(payload[5]-0) + 10*(payload[6]-0) + payload[7]-0;
+                drive(left_motor, right_motor); 
+            }
+            else if (payload[0] == '#') {
                 if(payload[1] == 'C') {
                   LED_ON;
                   wsSend(id, "Callibrating Magnetometer. Spin me please.");
